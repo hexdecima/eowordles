4c8a43a463dd12bbb964fa04f2d30590ae3802b3
Mai <mai@tilde.team>
2025-08-05 04:42:27 -0300

    feat(victory): add guess count

--- src/components/victory.rs
+++ src/components/victory.rs
@@ -12,11 +12,20 @@ pub fn victory() -> impl IntoView {
             .map_or("Unknown".to_string(), |e| e.name.to_string())
     };
 
+    let gueess_count = guess_reader.get().len();
+    let counter_text = if gueess_count > 1 {
+        format!("After {gueess_count} guesses.")
+    } else {
+        format!("First try, baby.")
+    };
+
     div().id("victory").child((
         b().child("That's it! The answer was"),
         p().child(format!("{}!", answer())),
         button().child("Back").on(ev::click, move |_| {
             ui.update(|ui| ui.show_victory = false);
-        })
+        }),
+        b().id("guesses-counter")
+            .child(counter_text),
     ))
 }
