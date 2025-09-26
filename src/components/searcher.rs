d999a32e45e8db04c36bc9efad6f85b4c85e8b31
Mai <mai@tilde.team>
2025-09-26 08:50:33 -0300

    half-done

--- src/components/searcher.rs
+++ src/components/searcher.rs
@@ -3,39 +3,129 @@ use leptos::{attr::Value, ev, html::*, prelude::*, task, IntoView};
 
 use crate::{api, data::Guess, GuessContext, UIManager};
 
+#[derive(Clone)]
+struct SearchManager {
+    /// Tracks all enemies to render in the search suggestion.
+    filtered: Vec<Enemy>,
+    /// What enemy (index) is selected.
+    selected: Option<usize>,
+    /// A reference to the selected search DOM item.
+    ref_selected: NodeRef<Button>
+}
+
+impl SearchManager {
+    /// Gets the amount of filtered items.
+    pub fn count(&self) -> usize {
+        self.filtered.len()
+    }
+
+    /// Checks if an item is selected by its index.
+    pub fn is_selected(&self, idx: usize) -> bool {
+        if let Some(selected) = self.selected {
+            selected == idx
+        } else { false }
+    }
+
+    /// Selects the next item, circularly.
+    pub fn next(&mut self) {
+        if self.count() == 0 {
+            return;
+        }
+
+        if let Some(selected) = self.selected {
+            let next_idx = selected + 1;
+
+            if next_idx > self.count() {
+                self.select(Some(0));
+            } else {
+                self.select(Some(next_idx));
+            }
+        } else {
+            self.select(Some(0));
+        }
+    }
+
+    /// Selects the previous item, circularly.
+    ///
+    /// Does nothing without items.
+    pub fn prev(&mut self) {
+        if self.count() == 0 {
+            return;
+        }
+
+        if let Some(selected) = self.selected {
+            let prev_idx = selected.checked_sub(1).unwrap_or(self.count() - 1);
+            self.select(Some(prev_idx));
+        } else {
+            self.select(Some(0));
+        }
+    }
+
+    pub fn select(&mut self, idx: Option<usize>) {
+        self.selected = idx;
+    }
+
+    /// Update the list of filtered enemies and reset the current selected one.
+    pub fn set_filtered(&mut self, new: Vec<Enemy>) {
+        self.filtered = new;
+        self.selected = Some(0);
+    }
+}
+
+impl Default for SearchManager {
+    fn default() -> Self {
+        Self { filtered: vec![], selected: None, ref_selected: NodeRef::new() }
+    }
+}
+
 pub fn searcher() -> impl IntoView {
     let enemies = list_enemies();
 
     let query = RwSignal::new(String::new());
-    let filtered = RwSignal::new(Vec::<Enemy>::new());
+    let search_mgr = RwSignal::new(SearchManager::default());
 
     Effect::new(move |_| {
-        if query.get().len() == 0 {
-            filtered.set(vec![]);
+        let filtered: Vec<Enemy> = if query.get().len() == 0 {
+            vec![]
         } else {
-            filtered.set(
                 enemies
                 .iter()
                 .filter(|e| e.name.to_lowercase().contains(&query.get()))
                 .cloned()
-                .collect(),
-            );
-        }
+                .collect()
+        };
+
+        search_mgr.update(|selection| selection.set_filtered(filtered));
     });
 
     div().class("searcher").child((
         input()
             .r#type("text")
             .id("searcher-input")
+            .on(ev::keydown, move |kb_ev| {
+                match kb_ev.key().as_str() {
+                    "ArrowUp" => search_mgr.update(|s| s.prev()),
+                    "ArrowDown" => search_mgr.update(|s| s.next()),
+                    _ => {}
+                }
+            })
             .value(query)
             .bind(Value, query)
-            .placeholder("Type here to search..."),
+            .placeholder("Type here to search... (To be implemented soon!)"),
         div().id("searcher-picker").child(move || {
-            filtered
+            search_mgr
                 .get()
+                .filtered
                 .iter()
                 .cloned()
-                .map(picker_item)
+                .enumerate()
+                .map(|(i, enemy)| {
+                    let mgr = search_mgr.write();
+                    let is_selected = search_mgr.get().is_selected(i);
+                    let item = picker_item(enemy, &is_selected);
+
+                    item
+                })
                 .collect_view()
         }),
     ))
@@ -43,17 +133,17 @@ pub fn searcher() -> impl IntoView {
 
 
 
-pub fn picker_item(enemy: Enemy) -> impl IntoView {
-    let text = format!("{} / {} life / {} defence / {}", enemy.name, enemy.life, enemy.defence, enemy.rarity);
-
+pub fn picker_item(enemy: Enemy, selected: bool) -> impl IntoView {
     let GuessContext(guess_reader, guess_writer) = use_context::<GuessContext>().unwrap();
     let ui = use_context::<RwSignal<UIManager>>().unwrap();
+    let class = if selected { "picker-item selected-item" } else { "picker-item" };
 
     button()
-        .class("picker-item")
+        .class(class)
         .child((
                 img().src(format!("assets/enemies/{}.gif", enemy.id)).aria_label(enemy.name.clone()),
-                text))
+                p().child(format!("{} life", enemy.life)),
+                p().child(format!("{}", enemy.rarity))))
         .on(ev::click, move |_| {
             let enemy = enemy.clone();
 
